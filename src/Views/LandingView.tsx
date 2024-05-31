bde80913f19079897221617797f2700fe0da846b
Lester (Vivobook) <lestercheong70+github@outlook.com>
2024-05-31 20:11:01 +0800

    fuck off

--- src/Views/LandingView.tsx
+++ src/Views/LandingView.tsx
@@ -1,7 +1,14 @@
 import { BasicLayout } from "../Layouts/Base/BasicLayout";
-import RetirementPlanDashboard from "./RetirementPlanDashboard";
 
 export const LandingView = () => {
   // TODO: we need to change this or at least redircet
-  return <BasicLayout mainContent={<RetirementPlanDashboard />} />;
+  return (
+    <BasicLayout
+      mainContent={
+        <>
+          <h1>IT FUCKING WORKS</h1>
+        </>
+      }
+    />
+  );
 };
